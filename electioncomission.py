1513c832775fd4676557cb1c860d7b22eae45605
RAINY\Rainy <rainyjain975@gmail.com>
2017-04-02 01:26:39 +0530

    added classification model k fold cross validation

--- electioncomission.py
+++ electioncomission.py
@@ -1,14 +1,29 @@
 #minor project prediction of candidate win
 #dataset :2014-2016 election commission dataset
 
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import KFold 
 import matplotlib.pyplot as plt
 
 def notNan(num):
     return num == num
+
+def classification_model(model, data, predictors, outcome):
+    model.fit(data[predictors],data[outcome])
+    predictions = model.predict(data[predictors])
+    accuracy = metrics.accuracy_score(predictions,data[outcome])
+    print("Accuracy : %s" % "{0:.3%}".format(accuracy))
+    kf = KFold(data.shape[0], n_folds=5)
+    error = []
+    for train, test in kf:
+        train_predictors = (data[predictors].iloc[train,:])
+        train_target = data[outcome].iloc[train]
+        model.fit(train_predictors, train_target)
+        error.append(model.score(data[predictors].iloc[test,:], data[outcome].iloc[test]))
+        print("Cross-Validation Score : %s" % "{0:.3%}".format(np.mean(error)))
+    model.fit(data[predictors],data[outcome]) 
 
 data = pd.read_csv("elec50.csv")
 print data.head(10)
@@ -37,25 +52,23 @@ data = data.drop(["Unnamed: 31"],axis=1)
 data = data.drop(["Unnamed: 32"],axis=1)
 data = data.drop(["Unnamed: 33"],axis=1)
 
-
 data = data[notNan(data.votes)]
-
 
 print data.gender.value_counts()
 data["gender"] = LabelEncoder().fit_transform(data["gender"].astype(str))
 print data.gender.value_counts()
 
-
 #plt.hist(data["gender"])
 #plt.show()
-
 
 #plt.scatter(data["age"],data["gender"])
 #plt.show()
 
-
 #data.plot(kind='density', subplots=True, layout=(3,3), sharex=False)
 #plt.show()
 
-data.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
-plt.show()
+#data.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+#plt.show()
+
+
+
