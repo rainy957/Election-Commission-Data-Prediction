3cb052664ae746a3bf8a1387a17f8a147ea5150b
RAINY\Rainy <rainyjain975@gmail.com>
2017-04-01 20:25:38 +0530

    read data

--- electioncomission.py
+++ electioncomission.py
@@ -1,2 +1,10 @@
 #minor project prediction of candidate win
 #dataset :2014-2016 election commission dataset
+
+
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("elec50.csv")
+print data.head(10)
+print data.describe()
